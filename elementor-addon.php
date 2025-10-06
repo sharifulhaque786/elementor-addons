32b1da147db8aad67dd2121b661dba633cf9969d
Md Shariful Haque <sharifulhaque786@gmail.com>
2025-10-06 04:10:32 +0400

    final

---
+++ elementor-addon.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name:      Elementor Test Addon
+ * Description:      Custom Elementor addon.
+ * Plugin URI:       https://elementor.com/
+ * Version:          1.0.0
+ * Author:           Elementor Developer
+ * Author URI:       https://developers.elementor.com/
+ * Text Domain:      elementor-test-addon
+ * Requires Plugins: elementor
+ */
+
+
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
